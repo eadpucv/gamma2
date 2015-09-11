4b324b5397f61656e008eb094164ca05088d0f9f
Idar Gonzalez <idargonzalez@gmail.com>
2015-09-11 16:34:19 -0300

    Se arregla las posiciones del botón 'editar' una vez que estás logueado como usuario con permisos

--- inc/widgets.php
+++ inc/widgets.php
@@ -21,26 +21,21 @@
                       echo '<i class="icn icn-lg rojo icn-anuncio icn-light en-linea izquierda margen-sup-negativo-sm"></i>';
                     echo '</div>';
                     echo '<div class="col-md-8 col-sm-4 borde radio-sup-der-sm radio-inf-der-sm sin-relleno-izq">';
-                      // echo '<i class="icn icn-lg rojo icn-anuncio icn-light en-linea izquierda"></i>';
-                      echo '<div class="h100 tooltip-demo">';
-                        // echo '<i class="icn icn-lg rojo icn-anuncio icn-light en-linea izquierda"></i>';
-                        echo '<h6 class="xs sin-margen sans rojo gruesa en-linea w60 altas">'.get_the_title( $the_post->ID ).'</h6>';
-                        echo '<p class="sin-margen"><span class="xs italica relleno-inf-xs">'.mysql2date('d \d\e F\, Y', $the_post->post_date).'</span></p>';
-                        if ( is_user_logged_in() ){
+                      echo '<div class="tooltip-demo">';
+                        echo '<h6 class="xs sin-margen sans rojo gruesa en-linea altas">'.get_the_title( $the_post->ID ).'</h6>';
+                        echo '<p class="sin-margen">';
+                            echo '<span class="xs italica relleno-inf-xs">'.mysql2date('d \d\e F\, Y', $the_post->post_date).'</span>';
+                            if ( is_user_logged_in() ){
                             echo '<a data-toggle="tooltip" title="Editar" class="xs en-linea rojo relleno-sup-xs sin-margen" href="'.get_edit_post_link($the_post->ID).'">';
                               echo '<span><i class="icn icn-lapiz margen-izq-xs margen-sup-xs"></i></span>';
                             echo '</a>';
                         }
+                        echo '</p>';
                       echo '</div>';
                       echo '<div class="der-lineal-xs">';
                         echo '<p class="xs sans sin-margen justificado">'.smart_substr( $the_post->post_content,255 ).'</p>';
                       echo '</div>';
                     echo '</div>';
-                    // echo '<div class="col-md-5 col-sm-12 relleno-izq-sm">';
-                    //   echo '<div class="der-lineal-xs">';
-                    //     echo '<p class="xs sans sin-margen justificado">'.smart_substr( $the_post->post_content,255 ).'</p>';
-                    //   echo '</div>';
-                    // echo '</div>';
                     echo '<div class="col-md-3 oculto-sm oculto-xs docencia-flex">';
                         echo '<a href="'.get_permalink( $the_post->ID ).'" class="btn centrado btn-alerta fondo-blanco sin-margen">Ver información</a>';
                     echo '</div>';
@@ -106,13 +101,14 @@ class WP_Widget_events extends WP_Widget {
                             echo '<div class="fecha"><span class="sm">'.mysql2date('d M',$event->event_date).'</span></div>';
                           echo '</div>';
                           echo '<div class="col-lg-9 col-md-9 col-sm-12 cabecera tooltip-demo">';
-                            echo '<h5 class="xs sin-margen relleno-inf-xs"><a href="'.get_permalink($event->ID).'">'.get_the_title($event->ID).'</a></h5>';
-                            // echo '<span class="xs entry-details">Publicado el '.mysql2date('d \d\e F\, Y',$event->event_date).'.</span>';
-                            if ( is_user_logged_in() ){
+                            echo '<h5 class="xs sin-margen relleno-inf-xs">';
+                                echo '<a href="'.get_permalink($event->ID).'">'.get_the_title($event->ID).' </a>';
+                                if ( is_user_logged_in() ){
                                 echo '<a data-toggle="tooltip" href="'.get_edit_post_link($event->ID).'" title="Editar" class="xs en-linea sin-margen">';
                                   echo '<span><i class="icn icn-lapiz icn-sm"></i></span>';
                                 echo '</a>';
                             } 
+                            echo '</h5>';
                             echo '</div>';
                         echo '</div>';
                         echo '<div class="col-lg-12 col-md-12 col-sm-12 contenido">';
@@ -311,7 +307,7 @@ class WP_Widget_posts extends WP_Widget {
                     // endif;
                       echo '<div class="auto margen-sup-sm margen-inf-sm tooltip-demo">';
                         echo '<h5 class="xs sin-margen relleno-inf-xs"><a href="'.get_permalink($the_post->ID).'">'.get_the_title($the_post->ID).'</a></h5>';
-                        echo '<span class="xs entry-details">Publicado en <a class="categoria" href="'.get_category_link($the_category->term_id).'">'.$the_category->name.'</a>, el '.mysql2date('d \d\e F\, Y',$the_post->post_date).'</span>';
+                        echo '<span class="xs entry-details">Publicado en <a class="categoria" href="'.get_category_link($the_category->term_id).'">'.$the_category->name.'</a>, el '.mysql2date('d \d\e F\, Y',$the_post->post_date).' </span> ';
                         if ( is_user_logged_in() ){
                           echo '<a data-toggle="tooltip" title="Editar" class="xs en-linea sin-margen" href="'.get_edit_post_link($the_post->ID).'">';
                             echo '<span><i class="icn icn-lapiz icn-sm"></i></span>';
