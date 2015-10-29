0440dc11ea38b607f4e50449aafe1b5951ca0d70
Hugo Solar <hugo.solar@gmail.com>
2015-10-29 16:59:43 -0300

    leer h4 y h2 cuando hay embeds de la wiki

--- js/script.js
+++ js/script.js
@@ -40,25 +40,19 @@ function string_to_slug(str) {
             });
         });
     }
-    $('.wiki-embed').find('h2').each(function(){
+
+    $('.page-contenido').find('h4, h2').each(function(){
         var obj = $(this);
-        var slug = string_to_slug(obj.text());
-        obj.attr('id',slug);
-        var icon = obj.find('.icn').clone().get(0);
-        var h6 = $('<h6>').addClass('xs').append(obj.text()).append(icon);
-        var link = $('<a>').attr('href','#'+slug).append(h6);
-        var li = $('<li>').addClass("sin-estilo").append(link);
-        $('.local-nav').append(li);
-    });
-    $('.page-contenido').find('h4').each(function(){
-        var obj = $(this);
-        var slug = string_to_slug(obj.text());
-        obj.attr('id',slug);
-        var icon = obj.find('.icn').clone().get(0);
-        var h6 = $('<h6>').addClass('xs').append(obj.text()).append(icon);
-        var link = $('<a>').attr('href','#'+slug).append(h6);
-        var li = $('<li>').addClass("sin-estilo").append(link);
-        $('.local-nav').append(li);
+        var tag_name = obj.prop('tagName');
+        if ( ( tag_name == 'H2' && obj.parent().hasClass('.wikiembed-fragment') ) || ( tag_name == 'H4' ) ) {
+            var slug = string_to_slug(obj.text());
+            obj.attr('id',slug);
+            var icon = obj.find('.icn').clone().get(0);
+            var h6 = $('<h6>').addClass('xs').append(obj.text()).append(icon);
+            var link = $('<a>').attr('href','#'+slug).append(h6);
+            var li = $('<li>').addClass("sin-estilo").append(link);
+            $('.local-nav').append(li);
+        }
     });
 
     $('#carousel-home, #carousel-enlaces, #carousel-portada').carousel();
